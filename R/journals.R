bd725ccdc837e1e9b7d26a600d4ccf94061b50f8
Will Pearse <will.pearse@gmail.com>
2015-07-07 17:10:00 -0400

    Added Wiley

--- R/journals.R
+++ R/journals.R
@@ -1,4 +1,3 @@
-
 #' Downloads supplementary materials from PLoS journals
 #' @param doi DOI of article
 #' @param si.no which supplement (first, second, etc.) as printed in
@@ -21,8 +20,7 @@
 plos <- function(doi, si.no, save.name=NULL, dir=NULL){
     #Argument handling
     if(!is.numeric(si.no))
-        stop("'si.no' must be numeric")
-    
+        stop("'si.no' must be numeric")    
     if(!is.null(dir)){
         if(!file.exists(dir))
             stop("'dir' must exist unless NULL")
@@ -40,7 +38,7 @@ plos <- function(doi, si.no, save.name=NULL, dir=NULL){
         stop("Unrecognised journal in DOI")
     journal <- journals[journal]
 
-    #Download
+    #Download and return
     destination <- file.path(dir, save.name)
     url <- paste0("http://journals.plos.org/", journal, "/article/asset?unique&id=info:doi/", doi, ".s", formatC(si.no, width=3, flag="0"))
     result <- download.file(url, destination, quiet=TRUE)
@@ -48,3 +46,49 @@ plos <- function(doi, si.no, save.name=NULL, dir=NULL){
         stop("Error code", result, " downloading file; file may not exist")
     return(destination)
 }
+
+#' Downloads supplementary materials from Wiley journals
+#' @param doi DOI of article
+#' @param si.no which supplement (first, second, etc.) as printed in
+#' the article, to download. Note that "Fig S1" is not valid; this
+#' should be a number
+#' @param save.name a name for the file to download. If \code{NULL}
+#' (default) this will be a combination of the DOI and SI number
+#' @param dir directory to save file to. If \code{NULL} (default) this
+#' will be a temporary directory created for your files
+#' @author Will Pearse
+#' @importFrom RCurl getURL
+#' @examples
+#' wiley("10.1111/ele.12437", 1)
+#' @export
+wiley <- function(doi, si.no, save.name=NULL, dir=NULL){
+    #Argument handling
+    if(!is.numeric(si.no))
+        stop("'si.no' must be numeric")    
+    if(!is.null(dir)){
+        if(!file.exists(dir))
+            stop("'dir' must exist unless NULL")
+    } else dir <- tempdir()
+    if(is.null(save.name)){
+        save.name <- paste(doi,si.no, sep="_")
+        save.name <- gsub(.Platform$file.sep, "_", save.name, fixed=TRUE)
+    }
+
+    #Download SI HTML page and find SI link
+    html <- getURL(paste0("http://onlinelibrary.wiley.com/doi/", doi, "/suppinfo"))
+    links <- gregexpr("(asset/supinfo/)[-0-9a-zA-Z\\.\\?\\=\\&\\,\\;]*", as.character(html), useBytes=FALSE)
+    pos <- as.numeric(links[[si.no]])
+    link <- substr(html, pos, pos+attr(links[[si.no]], "match.length")-1)
+    url <- paste0("http://onlinelibrary.wiley.com/store/", doi, "/", link)
+
+    #Download and return
+    destination <- file.path(dir, save.name)
+    result <- download.file(url, destination, quiet=TRUE)
+    if(result != 0)
+        stop("Error code", result, " downloading file; file may not exist")
+    return(destination)
+}
+
+elsevier
+
+http://www.sciencedirect.com.proxy3.library.mcgill.ca/science/article/pii/S0960982215005370
