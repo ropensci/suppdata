959f909b23fe49c06dcfd0104b97a06dfb870fba
Will Pearse <will.pearse@gmail.com>
2015-07-09 14:55:57 -0400

    Added Xeno Canto; package (should) now build from GitHHub

---
+++ R/utils.R
@@ -0,0 +1,19 @@
+#' Internal regexp functions
+.grep.url <- function(url, regexp, which=1){
+    html <- getURL(url)
+    return(.grep.text(html, regexp, which))
+}
+.grep.text <- function(text, regexp, which=1){
+    links <- gregexpr(regexp, text)
+    pos <- as.numeric(links[[1]][which])
+    return(substr(text, pos, pos+attr(links[[1]], "match.length")[which]-1))
+}
+
+#' Internal download function
+.download <- function(url, dir, save.name){
+    destination <- file.path(dir, save.name)
+    result <- download.file(url, destination, quiet=TRUE)
+    if(result != 0)
+        stop("Error code", result, " downloading file; file may not exist")
+    return(destination)
+}
