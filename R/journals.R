dafd3afdc9305022a9d28c724a28bb5bba682cda
Will Pearse <will.pearse@gmail.com>
2018-02-02 11:17:14 -0700

    Name change; fails checks but builds

--- R/journals.R
+++ R/journals.R
@@ -1,237 +1,165 @@
-#' Downloads supplementary materials from PLoS journals
-#' @param doi DOI of article
-#' @param si.no which supplement (first, second, etc.) as printed in
-#' the article, to download. Note that "Fig S1" is not valid; this
-#' should be a number
-#' @param save.name a name for the file to download. If \code{NULL}
-#' (default) this will be a combination of the DOI and SI number
-#' @param dir directory to save file to. If \code{NULL} (default) this
-#' will be a temporary directory created for your files
-#' @param cache if \code{TRUE} (default), the file won't be downloaded
-#' again if it already exists (in the temporary directory \code{grabr}
-#' creates, or your chosen \code{dir})
-#' @author Will Pearse
-#' @examples
-#' plos("10.1371/journal.pone.0127900", 1)
-#' plos("10.1371/journal.pbio.1002177", 1)
-#' plos("10.1371/journal.pmed.1001847", 1)
-#' plos("10.1371/journal.pgen.1005291", 1)
-#' plos("10.1371/journal.pcbi.1004327", 1)
-#' plos("10.1371/journal.ppat.1005005", 1)
-#' plos("10.1371/journal.pntd.0003824", 1)
-#' @export
-plos <- function(doi, si.no, save.name=NULL, dir=NULL, cache=TRUE){
+.suppdata.plos <- function(doi, si, save.name=NA, dir=NA, cache=TRUE, ...){
     #Argument handling
-    if(!is.numeric(si.no))
-        stop("'si.no' must be numeric")    
-    if(!is.null(dir)){
-        if(!file.exists(dir))
-            stop("'dir' must exist unless NULL")
-    } else dir <- tempdir()
-    if(is.null(save.name)){
-        save.name <- paste(doi,si.no, sep="_")
-        save.name <- gsub(.Platform$file.sep, "_", save.name, fixed=TRUE)
-    }
-
+    if(!is.numeric(si))
+        stop("PLoS download requires numeric SI info")
+    dir <- .tmpdir(dir)
+    save.name <- .save.name(doi, save.name, si)
+    
     #Find journal from DOI
     journals <- setNames(c("plosone", "plosbiology", "plosmedicine", "plosgenetics", "ploscompbiol", "plospathogens", "plosntds"), c("pone", "pbio", "pmed", "pgen", "pcbi", "ppat", "pntd"))
     journal <- gsub("[0-9\\.\\/]*", "", doi)
     journal <- gsub("journal", "", journal)
     if(sum(journal %in% names(journals)) != 1)
-        stop("Unrecognised journal in DOI")
+        stop("Unrecognised PLoS journal in DOI ", doi)
     journal <- journals[journal]
 
-    #Download and return
+#Download and return
     destination <- file.path(dir, save.name)
-    url <- paste0("http://journals.plos.org/", journal, "/article/asset?unique&id=info:doi/", doi, ".s", formatC(si.no, width=3, flag="0"))
+    url <- paste0("http://journals.plos.org/", journal, "/article/asset?unique&id=info:doi/", doi, ".s", formatC(si, width=3, flag="0"))
     return(.download(url, dir, save.name, cache))
 }
 
-#' Downloads supplementary materials from Wiley journals
-#' @param doi DOI of article
-#' @param si.no which supplement (first, second, etc.) as printed in
-#' the article, to download. Note that "Fig S1" is not valid; this
-#' should be a number
-#' @param save.name a name for the file to download. If \code{NULL}
-#' (default) this will be a combination of the DOI and SI number
-#' @param dir directory to save file to. If \code{NULL} (default) this
-#' will be a temporary directory created for your files
-#' @param cache if \code{TRUE} (default), the file won't be downloaded
-#' again if it already exists (in the temporary directory \code{grabr}
-#' creates, or your chosen \code{dir})
-#' @author Will Pearse
-#' @importFrom RCurl getURL
-#' @examples
-#' wiley("10.1111/ele.12437", 1)
-#' @export
-wiley <- function(doi, si.no, save.name=NULL, dir=NULL, cache=TRUE){
+#' @importFrom httr timeout
+#' @importFrom xml2 read_html xml_attr xml_find_all
+.suppdata.wiley <- function(doi, si, save.name=NA, dir=NA, cache=TRUE, timeout=10, ...){
     #Argument handling
-    if(!is.numeric(si.no))
-        stop("'si.no' must be numeric")    
-    if(!is.null(dir)){
-        if(!file.exists(dir))
-            stop("'dir' must exist unless NULL")
-    } else dir <- tempdir()
-    if(is.null(save.name)){
-        save.name <- paste(doi,si.no, sep="_")
-        save.name <- gsub(.Platform$file.sep, "_", save.name, fixed=TRUE)
-    }
+    if(!is.numeric(si))
+        stop("Wiley download requires numeric SI info")
+    dir <- .tmpdir(dir)
+    save.name <- .save.name(doi, save.name, si)
 
     #Download SI HTML page and find SI link
-    html <- getURL(paste0("http://onlinelibrary.wiley.com/doi/", doi, "/suppinfo"))
-    links <- gregexpr("(asset/supinfo/)[-0-9a-zA-Z\\.\\?\\=\\&\\,\\;_]*", as.character(html), useBytes=FALSE)
-    pos <- as.numeric(links[[si.no]])
-    link <- substr(html, pos, pos+attr(links[[si.no]], "match.length")-1)
-    url <- paste0("http://onlinelibrary.wiley.com/store/", doi, "/", link)
-
+    # - requires check for new Ecology Letters page (...the page seems buggy...)
+    html <- tryCatch(as.character(GET(paste0("http://onlinelibrary.wiley.com/doi/", doi, "/full"), httr::timeout(timeout))),
+                     silent=TRUE, error = function(x) NA)
+    if(is.na(html))
+        html <- as.character(GET(paste0("http://onlinelibrary.wiley.com/wol1/doi/", doi, "/full"), httr::timeout(timeout)))
+    links <- gregexpr("(asset/supinfo/)[-0-9a-zA-Z\\.\\?\\=\\&\\,\\;_]*", html, useBytes=FALSE)
+    if(any(links[[1]] == -1))
+        links <- gregexpr("(asset/supinfo)[-0-9a-zA-Z\\.\\?\\=\\&\\,\\;_%]*", html, useBytes=FALSE)
+    
+    html <- read_html(html)
+    urls <- xml_attr(xml_find_all(html, '//a[contains(@href,"supinfo")]'), "href")
+    if(si > length(urls))
+        stop("SI number '", si, "' greater than number of detected SIs (", length(urls), ")")
+    url <- urls[si]
+    
     #Download and return
     destination <- file.path(dir, save.name)
     return(.download(url, dir, save.name, cache))
 }
 
-#' Downloads supplementary materials from FigShare
-#' @param doi DOI of article
-#' @param si.no which supplement (first, second, etc.) as printed in
-#' the article, to download. Note that "Fig S1" is not valid; this
-#' should be a number
-#' @param save.name a name for the file to download. If \code{NULL}
-#' (default) this will be a combination of the DOI and SI number
-#' @param dir directory to save file to. If \code{NULL} (default) this
-#' will be a temporary directory created for your files
-#' @param cache if \code{TRUE} (default), the file won't be downloaded
-#' again if it already exists (in the temporary directory \code{grabr}
-#' creates, or your chosen \code{dir})
-#' @author Will Pearse
-#' @examples
-#' figshare("10.6084/m9.figshare.979288", 1)
-#' @export
-figshare <- function(doi, si.no, save.name=NULL, dir=NULL, cache=TRUE){
+#' @importFrom jsonlite fromJSON
+#' @importFrom xml2 xml_text xml_find_one
+.suppdata.figshare <- function(doi, si, save.name=NA, dir=NA, cache=TRUE, ...){
     #Argument handling
-    if(!is.numeric(si.no))
-        stop("'si.no' must be numeric")    
-    if(!is.null(dir)){
-        if(!file.exists(dir))
-            stop("'dir' must exist unless NULL")
-    } else dir <- tempdir()
-    if(is.null(save.name)){
-        save.name <- paste(doi,si.no, sep="_")
-        save.name <- gsub(.Platform$file.sep, "_", save.name, fixed=TRUE)
-    }
-
+    if(!(is.numeric(si) | is.character(si)))
+        stop("FigShare download requires numeric or character SI info")
+    dir <- .tmpdir(dir)
+    save.name <- .save.name(doi, save.name, si)
+    
     #Find, download, and return
-    url <- .grep.url(paste0("http://dx.doi.org/", doi), "(http://figshare.com/articles/)[A-Za-z0-9_/]*")
-    url <- .grep.url(url, "(http://files\\.figshare\\.com/)[-a-zA-Z0-9\\_/\\.]*", si.no)
-    return(.download(url, dir, save.name, cache))
+    html <- read_html(content(GET(paste0("http://dx.doi.org/", doi)), "text"))
+    results <- fromJSON(xml_text(xml_find_one(html, "//script[@type=\"text/json\"]")))$article$files
+    if(is.numeric(si)){
+        if(si > nrow(results))
+            stop("SI number '", si, "' greater than number of detected SIs (", nrow(results), ")")
+        return(.download(results$downloadUrl[si], dir, save.name, cache))
+    }
+    if(!si %in% results$name)
+        stop("SI name not in files on FigShare (which are: ", paste(results$name,collapse=","), ")")
+    return(.download(results$downloadUrl[results$name==si], dir, save.name, cache))
 }
 
-#' Downloads supplementary materials from Ecological Archives
-#' @param esa ESA code for the article (just below the title in the
-#' manuscript)
-#' @param si.name *name* of the file to be downloaded
-#' @param save.name a name for the file to download. If \code{NULL}
-#' (default) this will be a combination of the DOI and SI number
-#' @param dir directory to save file to. If \code{NULL} (default) this
-#' will be a temporary directory created for your files
-#' @param data.paper whether paper is an ESA data paper (default:
-#' TRUE), or you are attempting to download the supplement to a
-#' standard paper (e.g., one in Ecology)
-#' @param cache if \code{TRUE} (default), the file won't be downloaded
-#' again if it already exists (in the temporary directory \code{grabr}
-#' creates, or your chosen \code{dir})
-#' @author Will Pearse
-#' @examples
-#' fungi <- read.csv(esa.archives("E093-059", "myco_db.csv"))
-#' mammals <- read.csv(esa.archives("E092-201", "MCDB_communities.csv", FALSE))
-#' @export
-esa.archives <- function(esa, si.name, data.paper=TRUE, save.name=NULL, dir=NULL, cache=TRUE){
+.suppdata.esa_data_archives <- function(esa, si, save.name=NA, dir=NA, cache=TRUE, ...){
     #Argument handling
-    if(data.paper)
-        data.paper <- "" else data.paper <- "/data"
-    if(!is.character(si.name))
-        stop("'si.no' must be a character")    
-    if(!is.null(dir)){
-        if(!file.exists(dir))
-            stop("'dir' must exist unless NULL")
-    } else dir <- tempdir()
-    if(is.null(save.name)){
-        save.name <- paste(esa,si.name, sep="_")
-        save.name <- gsub(.Platform$file.sep, "_", save.name, fixed=TRUE)
-    }
+    if(!is.character(si))
+        stop("ESA Archives download requires character SI info")
+    dir <- .tmpdir(dir)
+    save.name <- .save.name(doi, save.name, si)
 
     #Download, and return
     esa <- gsub("-", "/", esa, fixed=TRUE)
-    return(.download(paste0("http://esapubs.org/archive/ecol/", esa, data.paper, "/", si.name), dir, save.name, cache))
+    return(.download(paste0("http://esapubs.org/archive/ecol/", esa, "/data", "/", si), dir, save.name, cache))
+}
+.suppdata.esa_archives <- function(esa, si, save.name=NA, dir=NA, cache=TRUE, ...){
+    #Argument handling
+    if(!is.character(si))
+        stop("ESA Archives download requires character SI info")
+    dir <- .tmpdir(dir)
+    save.name <- .save.name(doi, save.name, si)
+
+    #Download, and return
+    esa <- gsub("-", "/", esa, fixed=TRUE)
+    return(.download(paste0("http://esapubs.org/archive/ecol/", esa, "/", si), dir, save.name, cache))
 }
 
-#' Downloads supplementary materials from Science
-#' @param doi DOI of article
-#' @param si.name *name* of the file to be downloaded
-#' @param save.name a name for the file to download. If \code{NULL}
-#' (default) this will be a combination of the DOI and SI number
-#' @param dir directory to save file to. If \code{NULL} (default) this
-#' will be a temporary directory created for your files
-#' @param cache if \code{TRUE} (default), the file won't be downloaded
-#' again if it already exists (in the temporary directory \code{grabr}
-#' creates, or your chosen \code{dir})
-#' @author Will Pearse
-#' @examples
-#' science("10.1126/science.1255768", "Appendix_BanksLeite_etal.txt")
-#' @export
-science <- function(doi, si.name, save.name=NULL, dir=NULL, cache=TRUE){
+.suppdata.science <- function(doi, si, save.name=NA, dir=NA, cache=TRUE, ...){
     #Argument handling
-    if(!is.character(si.name))
-        stop("'si.name' must be a character")    
-    if(!is.null(dir)){
-        if(!file.exists(dir))
-            stop("'dir' must exist unless NULL")
-    } else dir <- tempdir()
-    if(is.null(save.name)){
-        save.name <- paste(doi,si.name, sep="_")
-        save.name <- gsub(.Platform$file.sep, "_", save.name, fixed=TRUE)
-    }
+    if(!is.character(si))
+        stop("Science download requires character SI info")
+    dir <- .tmpdir(dir)
+    save.name <- .save.name(doi, save.name, si)
 
     #Find, download, and return
     url <- paste0("http://www.sciencemag.org", .grep.url(paste0("http://www.sciencemag.org/lookup/doi/", doi), "(/content/)[0-9/]*"), "/suppl/DC1")
-    url <- paste0("http://www.sciencemag.org", .grep.url(url, "(/content/suppl/)[A-Z0-9/\\.]*(Appendix_BanksLeite_etal.txt)"))
+    url <- paste0("http://www.sciencemag.org", .grep.url(url, "(/content/suppl/)[A-Z0-9/\\.]*"))
     return(.download(url, dir, save.name, cache))
 }
 
-#' Downloads supplementary materials from Proceedings journals
-#' @param doi DOI of article
-#' @param vol volume of article
-#' @param issue issue of article
-#' @param si.no which supplement (first, second, etc.) as printed in
-#' the article, to download. Note that "Fig S1" is not valid; this
-#' should be a number
-#' @param save.name a name for the file to download. If \code{NULL}
-#' (default) this will be a combination of the DOI and SI number
-#' @param dir directory to save file to. If \code{NULL} (default) this
-#' will be a temporary directory created for your files
-#' @param cache if \code{TRUE} (default), the file won't be downloaded
-#' again if it already exists (in the temporary directory \code{grabr}
-#' creates, or your chosen \code{dir})
-#' @author Will Pearse
-#' @importFrom RCurl getURL
-#' @examples
-#' proceedings("10.1098/rspb.2015.0338", 282, 1811, 1)
-#' @export
-proceedings <- function(doi, si.no, vol, issue, save.name=NULL, dir=NULL, cache=TRUE){
+.suppdata.proceedings <- function(doi, si, vol, issue, save.name=NA, dir=NA, cache=TRUE, ...){
     #Argument handling
-    if(!is.numeric(si.no))
-        stop("'si.no' must be numeric")    
-    if(!is.null(dir)){
-        if(!file.exists(dir))
-            stop("'dir' must exist unless NULL")
-    } else dir <- tempdir()
-    if(is.null(save.name)){
-        save.name <- paste(doi,si.no, sep="_")
-        save.name <- gsub(.Platform$file.sep, "_", save.name, fixed=TRUE)
-    }
-
+    if(!is.numeric(si))
+        stop("Proceedings download requires numeric SI info")
+    dir <- .tmpdir(dir)
+    save.name <- .save.name(doi, save.name, si)
+    
     #Find, download, and return
     journal <- .grep.text(doi, "(rsp)[a-z]")
     tail <- gsub(".", "", .grep.text(doi, "[0-9]+\\.[0-9]*", 2), fixed=TRUE)
     url <- paste0("http://", journal, ".royalsocietypublishing.org/content/", vol, "/", issue, "/", tail, ".figures-only")
     url <- paste0("http://rspb.royalsocietypublishing.org/", .grep.url(url, "(highwire/filestream)[a-zA-Z0-9_/\\.]*"))
     return(.download(url, dir, save.name))
+}
+
+#' @importFrom xml2 xml_text xml_find_one read_xml
+.suppdata.epmc <- function(doi, si, save.name=NA, dir=NA, cache=TRUE, list=FALSE, ...){
+    #Argument handling
+    if(!is.character(si))
+        stop("EPMB download requires numeric SI info")
+    dir <- .tmpdir(dir)
+    save.name <- .save.name(doi, save.name, si)
+    zip.save.name <- .save.name(doi, NA, "raw_zip.zip")
+    
+    #Find, download, and return
+    pmc.id <- xml_text(xml_find_one(read_xml(paste0("http://www.ebi.ac.uk/europepmc/webservices/rest/search/query=", doi)), ".//pmcid"))
+    url <- paste0("http://www.ebi.ac.uk/europepmc/webservices/rest/", pmc.id[[1]], "/supplementaryFiles")
+    zip <- tryCatch(.download(url,dir,zip.save.name,cache), error=function(x) stop("Cannot find supplementary materials for (seemingly) valid EPMC article ID ",pmc.id[[1]]))
+    return(.unzip(zip, dir, save.name, cache, si, list))
+}
+
+.suppdata.biorxiv <- function(doi, si, save.name=NA, dir=NA, cache=TRUE, ...){
+    #Argument handling
+    if(!is.numeric(si))
+        stop("bioRxiv download requires numeric SI info")
+    dir <- .tmpdir(dir)
+    save.name <- .save.name(doi, save.name, si)
+    
+    #Find, download, and return
+    url <- paste0(.url.redir(paste0("http://dx.doi.org/", doi)), ".figures-only")
+    file <- .grep.url(url, "/highwire/filestream/[a-z0-9A-Z\\./_-]*", si)
+    return(.download(.url.redir(paste0("http://biorxiv.org",file)), dir, save.name, cache))
+}
+
+.suppdata.dryad <- function(doi, si, save.name=NA, dir=NA, cache=TRUE, ...){
+    #Argument handling
+    if(!is.character(si))
+        stop("DataDRYAD download requires numeric SI info")
+    dir <- .tmpdir(dir)
+    save.name <- .save.name(doi, save.name, si)
+    
+    #Find, download, and return
+    url <- .url.redir(paste0("http://dx.doi.org/", doi))
+    file <- .grep.url(url, paste0("/bitstream/handle/[0-9]+/dryad\\.[0-9]+/", URLencode(si,reserved=TRUE)))
+    return(.download(.url.redir(paste0("http://datadryad.org",file)), dir, save.name, cache))
 }
--- R/utils.R
+++ R/utils.R
@@ -1,21 +1,128 @@
-#' Internal regexp functions
+.suppdata.pub <- function(x){
+    if(!is.character(x))
+        stop("'x' must be a character")
+
+    #Doing the check here saves one internet call
+    if(grepl("figshare", x))
+        return("figshare")
+    if(grepl("dryad", x))
+        return("dryad")
+    pub <- cr_works(x)$data
+    
+    if(pub$prefix=="http://id.crossref.org/prefix/10.0000")
+        stop("Cannot find publisher for DOI: ", x)
+    
+    return(.grep.text(pub$member, "[0-9]+"))
+}
+
+.suppdata.func <- function(x) {
+    #Check by code, return if found
+    output <- switch(x, 
+                     "340" = get_si_plos,
+                     "311" = get_si_wiley,
+                     "221" = get_si_science,
+                     "175" = get_si_proceedings,
+                     "246" = get_si_biorxiv
+                     )
+    if(!is.null(output))
+        return(output)
+
+    #Check by letter code
+    output <- switch(x, 
+                     "plos" = get_si_plos,
+                     "wiley" = get_si_wiley,
+                     "science" = get_si_science,
+                     "proceedings" = get_si_proceedings,
+                     "figshare" = get_si_figshare,
+                     "esa_data_archives" = get_si_esa_data_archives,
+                     "esa_archives" = get_si_esa_archives,
+                     "biorxiv" = get_si_biorxiv,
+                     "epmc" = get_si_epmc,
+                     "dryad" = get_si_dryad
+                     )
+    #If all else fails, try EPMC
+    if(is.null(output))
+        output <- get_si_epmc
+    return(output)
+}
+
+# Internal regexp functions
 .grep.url <- function(url, regexp, which=1){
-    html <- getURL(url)
+    html <- as.character(GET(url))
     return(.grep.text(html, regexp, which))
 }
 .grep.text <- function(text, regexp, which=1){
     links <- gregexpr(regexp, text)
+    if(which > length(links[[1]]))
+        stop("SI number '", which, "' greater than number of detected SIs (", length(links[[1]]), ")")
     pos <- as.numeric(links[[1]][which])
     return(substr(text, pos, pos+attr(links[[1]], "match.length")[which]-1))
 }
+.file.suffix <- function(text, max.length=4){
+    suffix <- .grep.text(text, "[a-zA-Z]+$")
+    if(nchar(suffix) <= max.length & nchar(suffix) > 0)
+        return(suffix)
+    return(NA)
+}
 
-#' Internal download function
+# Internal download function
 .download <- function(url, dir, save.name, cache=TRUE){
     destination <- file.path(dir, save.name)
-    if(cache==TRUE & file.exists(destination))
+    suffix <- .file.suffix(url, 4)
+    
+    if(cache==TRUE & file.exists(destination)){
+        if(!is.na(suffix))
+            attr(destination, "suffix") <- suffix
         return(destination)
+    }
+    
     result <- download.file(url, destination, quiet=TRUE)
     if(result != 0)
         stop("Error code", result, " downloading file; file may not exist")
+    
+    if(!is.na(suffix))
+        attr(destination, "suffix") <- suffix
     return(destination)
+}
+
+# Internal unzip function
+.unzip <- function(zip, dir, save.name, cache, si, list=FALSE){
+    files <- unzip(zip, list=TRUE)
+    if(list){
+        cat("Files in ZIP:")
+        print(files)
+    }
+    if(!si %in% files$Name)
+        stop("Required file not in zipfile ", zip)
+    file <- unzip(zip, si)
+    file.rename(file, file.path(dir, save.name))
+    return(file.path(dir, save.name))
+}
+
+# Internal URL 'redirect' function
+.url.redir <- function(x)
+    return(GET(x)$url)
+
+.tmpdir <- function(dir){
+    if(!is.na(dir)){
+        if(!file.exists(dir))
+            stop("'dir' must exist unless NA")
+    } else dir <- tempdir()
+    return(dir)
+}
+.save.name <- function(doi, save.name, file){
+    if(is.na(save.name)){
+        save.name <- paste(doi,file, sep="_")
+        save.name <- gsub(.Platform$file.sep, "_", save.name, fixed=TRUE)
+    }
+    return(save.name)
+}
+#Expanding arguments when handling ft-classes
+.fix.param <- function(x, param, name){
+    if(length(x) != length(param)){
+        if((length(x) %% length(param)) != 0)
+            stop("length of ", "name (", length(param), ") is incompatible with 'x' (", length(x), ")")
+        param <- rep(param, length(x))
+    }
+    return(param)
 }
